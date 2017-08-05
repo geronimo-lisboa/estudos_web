812ff197590ed4952ab90d6ba93790e0678dcbb6
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-08-05 18:28:01 -0300

    Contexto funcionando.

---
+++ public/opengl.js
@@ -0,0 +1,22 @@
+﻿//Cria o contexto do webgl
+function initWebGL(canvas) {
+    var gl;
+    try {
+        gl = canvas.getContext("experimental-webgl");
+    }
+    catch (e) {
+        var msg = "Erro na criação do contexto: " + e.toString();
+        alert(msg);
+        throw Error(msg);
+    }
+    return gl;
+}
+//Cria a viewport
+function initViewport(gl, canvas) {
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+//Calcula o FPS e exibe na div dada.
+function showFPS(t0, t, targetDiv) {
+    const fps = 1000.0 / ((t - t0) === 0 ? 1 : (t - t0));
+    targetDiv.text("FPS = " + fps);
+}
