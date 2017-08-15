99221e91b643556c2135fbcaf8ff8947d3df0dcf
Ubuntu <ubuntu@ip-172-31-11-123.us-west-2.compute.internal>
2017-08-15 02:49:38 +0000

    funcionando na aws

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var express = require('express');
 //Cria o objeto do servidor usando a biblioteca
 var server = express(); 
 server.use(express.static(__dirname + '/public'));
+console.log('hello world, começando server');
+//var HOST = 'ec2-54-200-54-146.us-west-2.compute.amazonaws.com';
+//var PORT = 8080;
 server.listen(8080);
